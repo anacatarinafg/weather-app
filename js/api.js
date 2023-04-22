1a383e3356a1b97140b1a2b3b778537ffe75916f
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-04-22 23:52:24 +0100

    fetching the api and get info about the date

---
+++ js/api.js
@@ -0,0 +1,32 @@
+const API_KEY = "cc92ecf94efe3771ab1d412b9b997d67";
+
+/**
+ * Fetch data from the server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function (URL, callback) {
+  console.log(`${URL}&appid=${API_KEY}`);
+  console.log("ola")
+  fetch(`${URL}&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => callback(data));
+};
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=10`;
+  },
+};
